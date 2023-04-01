2629177a493268416b42b61db7156715522fec0d
Carlos <128559215+carlossalgado1@users.noreply.github.com>
2023-04-01 16:48:35 -0300

    Update views.py

--- base/views.py
+++ base/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django import forms
 from.models import Tarea
 
 
